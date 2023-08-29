616f272fb5c0405b6a20b749f977ad6bbdff13ec
NORIKIM <jinagold.k@gmail.com>
2023-08-29 22:52:42 +0900

    메인 뷰컨트롤러 구현

--- Marvel/Marvel/View/MainVC.swift
+++ Marvel/Marvel/View/MainVC.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    lazy var favoriteBTN: UIBarButtonItem = {
+        let img = UIImage(named: "favorite")!
+        let button = UIBarButtonItem(image: img, style: .done, target: self, action: #selector(moveToFavorite(_:)))
+        
+        return button
+    }()
     private let viewModel = MainVM()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -21,6 +29,14 @@ class MainVC: UIViewController {
         viewModel.reset()
         viewModel.reload()
     }
-
+    
+    func setUI() {
+        self.navigationItem.rightBarButtonItem = favoriteBTN
+        self.navigationItem.rightBarButtonItem?.tintColor = .red
+    }
+    
+    @objc func moveToFavorite(_ sender: UIBarButtonItem) {
+        
+    }
 }
 
